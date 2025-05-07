7a7cd946546c6c351e064713fadc257d2ac5f5ca
Shinya Kato <shinya11.kato@gmail.com>
2025-05-07 11:07:23 +0900

    Fix 04_pitr/cleanup.sh

--- 04_pitr/cleanup.sh
+++ 04_pitr/cleanup.sh
@@ -7,6 +7,10 @@ gcloud container clusters delete cnds2025-04-pitr \
 
 gcloud storage rm --recursive gs://cnds2025-04-pitr
 
+gcloud projects remove-iam-policy-binding cnds2025-db-on-k8s \
+  --member serviceAccount:cnds2025-04-pitr-sa@cnds2025-db-on-k8s.iam.gserviceaccount.com \
+  --role roles/storage.admin
+
 gcloud iam service-accounts delete cnds2025-04-pitr-sa@cnds2025-db-on-k8s.iam.gserviceaccount.com \
   --quiet
 
